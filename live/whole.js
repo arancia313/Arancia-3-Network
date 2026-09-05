e36d5263670edfdbf05cfc27f6ef88b162ee1de1
Arancia 3 Technologies <marraalan89@gmail.com>
2026-09-05 16:58:08 +0200

    Here.

--- live/whole.js
+++ live/whole.js
@@ -1,2 +1,3 @@
-const url = "https://raw.githubusercontent.com/arancia313/Arancia-3-Network/refs/heads/System/net/A3N_Announcements/net_announcements.txt"
+const url = "https://raw.githubusercontent.com/arancia313/Arancia-3-Network/refs/heads/System/net/A3N_Announcements/net_announcements.txt";
 fetch(url).then(r=>r.text()).then(t=>document.getElementById("view").innerText=t);
+document.getElementById("audioon").play();
