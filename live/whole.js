e8b2ec8c399d7fda62ac350c37262080f87d2a07
Arancia 3 Technologies <marraalan89@gmail.com>
2026-09-05 14:06:40 +0200

    Changed the ENTIRE point of A3N Live.

--- live/whole.js
+++ live/whole.js
@@ -1,10 +1,2 @@
-fetch('https://raw.githubusercontent.com/arancia313/Arancia-3-Network/refs/heads/System/users.json')
-.then(res => res.json())
-.then(data => {
-    var html = '';
-    for (var key in data.users) {
-        html += '<p>' + key + ': ' + JSON.stringify(data.users[key]) + '</p>';
-    }
-    document.getElementById('view').innerHTML = html;
-})
-.catch(error => console.error('Parsing Error;', error))
+const url = "https://raw.githubusercontent.com/arancia313/Arancia-3-Network/refs/heads/System/net/A3N_Announcements/net_announcements.txt"
+fetch(url).then(r=>r.text()).then(t=>document.getElementById("view").innerText=t);
