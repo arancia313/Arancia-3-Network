7213b9a11526a0d8a4b1f57b75c4c3d9bab33e5e
Arancia 3 Technologies <marraalan89@gmail.com>
2026-09-05 16:52:44 +0200

    Here. A little quick fix.

--- live/whole.js
+++ live/whole.js
@@ -1,4 +1,4 @@
 const url = "https://raw.githubusercontent.com/arancia313/Arancia-3-Network/refs/heads/System/net/A3N_Announcements/net_announcements.txt";
 fetch(url).then(r=>r.text()).then(t=>document.getElementById("view").innerText=t);
 const url2 = "https://raw.githubusercontent.com/arancia313/Arancia-3-Network/refs/heads/System/net/platform_updates/A3N_Live/updates.txt";
-fetch(url2).then(r=>r.text()).then(t=>document.getElementById("view").innerText=t);
+fetch(url2).then(r=>r.text()).then(t=>document.getElementById("pa").innerText=t);
