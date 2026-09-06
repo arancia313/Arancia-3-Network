abad7671acc1734b5ec197864fa8ec5345f6f20e
Arancia 3 Technologies <marraalan89@gmail.com>
2026-09-06 11:06:42 +0200

    Ever since i added those comments, i want them compact.

--- users.js
+++ users.js
@@ -1,8 +1,7 @@
     const ccj = "ccj" + 8;
     const ranks = ["Non-Admin" = 0, "VIP" = 1, "Mod" = 2, "Admin" = 3, "HeadAdmin" = 4, "Owner" = 5];
     const permissions = ["none", "ban", "write"];
-    const users = [
-    // For the users.
+    const users = [ // For the users.
     {id: 1, name: "Arancia_Carnivora", onlineID: "Arancia_Carnivora", rank: ranks = 5, status: "offline", hash: "#0", content: "hidden", ccj: ccj},
     {id: 2, name: "arancia_ahh", onlineID:  "arancia_ahh", rank: ranks = 5, status: "online", hash: "#1", permissions: "write"},
     {id: 3, name: "pecete dev", onlineID: "pecete_dev", rank: ranks = 4, status: "offline", hash: "#2"},
@@ -12,8 +11,7 @@
     {id: 7, name: "providers", onlineID: "providers", rank: ranks = 4, status: "offline", hash: "#6"},
     {id: 8, name: "Marsh", onlineID: "marsh_", rank: ranks = 2, status: "offline", hash: "#7"},
     {id: 9, name: "player3540", onlineID: "player3540", rank: ranks = 0, status: "offline", hash: "#8"}];
-    const bots = [
-    // For the bots and the service provider.
+    const bots = [ // For the bots and the service provider.
     {id: "@+" + 1, name: "Arancia 3 Network Service Provider", onlineID: "[A3N]", rank: ranks = 4, status: "offline", deadlineID: "23479394231798342748", hash: null},
     {id: "@+" + 2, name: "celdrone", onlineID: "celdrone", rank: ranks = 0, status: "offline", hash: "#+1"},
     {id: "@+" + 3, name: "ostash", onlineID: "ostash", rank: ranks = 0, status: "offline", hash: "#+2"},
