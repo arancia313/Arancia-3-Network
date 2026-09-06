6a683439168d3b9dee714f1ffab17b4582b1fc57
Arancia 3 Technologies <marraalan89@gmail.com>
2026-09-06 10:15:21 +0200

    Updated A3N Live. I rolled out the hash for it.

--- live/whole.js
+++ live/whole.js
@@ -1,3 +1,6 @@
+const hash = "#" + Math.random(2 -2);
+console.log("Starting Shell with A3N Live hash:", hash);
+document.getElementById("hash").innerText = hash;
 const url = "https://raw.githubusercontent.com/arancia313/Arancia-3-Network/refs/heads/System/net/A3N_Announcements/net_announcements.txt";
 fetch(url).then(r=>r.text()).then(t=>document.getElementById("view").innerText=t);
 const url2 = "https://raw.githubusercontent.com/arancia313/Arancia-3-Network/refs/heads/System/net/platform_updates/A3N_Live/updates.txt";
