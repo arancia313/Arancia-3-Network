746f693e1c33ed102ec7cd21b8d3dd6d40b21eeb
Arancia 3 Technologies <marraalan89@gmail.com>
2026-09-06 10:55:43 +0200

    Added OTHER user objects.

--- users.js
+++ users.js
@@ -1,12 +1,16 @@
-const ranks = ["Non-Admin" = 0, "VIP" = 1, "Mod" = 2, "Admin" = 3, "HeadAdmin" = 4, "Owner" = 5]
-const users = [
-    {id: 1, name: "Arancia_Carnivora", onlineID: "Arancia_Carnivora", rank: ranks = 5, status: "offline", hash: "#0", content: "hidden"},
-    {id: 2, name: "arancia_ahh", onlineID:  "arancia_ahh", rank: ranks = 5, status: "online", hash: "#1"},
+    const ccj = "ccj" + 8;
+    const ranks = ["Non-Admin" = 0, "VIP" = 1, "Mod" = 2, "Admin" = 3, "HeadAdmin" = 4, "Owner" = 5];
+    const permissions = ["none", "ban", "write"];
+    const users = [
+    {id: 1, name: "Arancia_Carnivora", onlineID: "Arancia_Carnivora", rank: ranks = 5, status: "offline", hash: "#0", content: "hidden", ccj: ccj},
+    {id: 2, name: "arancia_ahh", onlineID:  "arancia_ahh", rank: ranks = 5, status: "online", hash: "#1", permissions: "write"},
     {id: 3, name: "pecete dev", onlineID: "pecete_dev", rank: ranks = 4, status: "offline", hash: "#2"},
     {id: 4, name: "arancia313", onlineID: "arancia313", rank: ranks = 5, status: "offline", hash: "#3"},
     {id: 5, name: "Player 3365", onlineID: "player3365", rank: ranks = 3, status: "offline", hash: "#4"},
     {id: 6, name: "someone", onlineID: "someone", rank: ranks = 1, status: "offline", hash: "#5"},
-    {id: 7, name: "providers", onlineID: "providers", rank: ranks = 3, status: "offline", hash: "#6"}];
+    {id: 7, name: "providers", onlineID: "providers", rank: ranks = 3, status: "offline", hash: "#6"},
+    {id: 8, name: "Marsh", onlineID: "marsh_", rank: ranks = 2, status: "offline", hash: "#7"},
+    {id: 9, name: "player3540", onlineID: "player3540", rank: ranks = 0, status: "offline", hash: "#8"}];
     const bots = [
     {id: "@+" + 1, name: "Arancia 3 Network Service Provider", onlineID: "[A3N]", rank: ranks = 2, status: "offline", deadlineID: "23479394231798342748", hash: null},
     {id: "@+" + 2, name: "celdrone", onlineID: "celdrone", rank: ranks = 0, status: "offline", hash: "#+1"},
@@ -15,4 +19,6 @@ const users = [
     {id: "@+" + 5, name: "Service 2", onlineID: "service-2", rank: ranks = 3, status: "offline", hash: "#+4"},
     {id: "@+" + 6, name: "Service 3", onlineID: "service-3", rank: ranks = 3, status: "offline", hash: "#+5"},
     {id: "@+" + 7, name: "A3N Live", onlineID: "A3N-Live", rank: ranks = 0, status: "offline", hash: "#+6"},
-    {id: "@+" + 8, name: "ghost", onlineID: "ghost_", rank: ranks = 3, status: "offline", hash: "#+7"}];
+    {id: "@+" + 8, name: "ghost", onlineID: "ghost_", rank: ranks = 3, status: "offline", hash: "#+7"},
+    {id: "@+" + 9, name: "player4894", onlineID: "player4894", rank: ranks = 0, status: "offline", hash: "#+8"},
+    {id: "@+" + 10, name: "player5934", onlineID: "player5934", rank: ranks = 0, status: "offline", hash: "#+9"}];
