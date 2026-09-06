cd8d7967c61af39f512dd51d12b98dbf1a7b2cb8
Arancia 3 Technologies <marraalan89@gmail.com>
2026-09-06 09:23:00 +0200

    The first thing i don't want to miss out: The hashes and the user IDs.

--- users.js
+++ users.js
@@ -1,4 +1,14 @@
-const admin_accounts = [
-    {id: 1, name: "arancia_ahh", rank: 5, status: "online"},
-    {id: 2, name: "pecete dev", rank: 4, status: "online"}
-]
+const ranks = ["Non-Admin" = 0, "VIP" = 1, "Mod" = 2, "Admin" = 3, "HeadAdmin" = 4, "Owner" = 5]
+const users = [
+    {id: 1, name: "Arancia_Carnivora", onlineID: "Arancia_Carnivora", rank: ranks = 5, status: "offline", hash: "#0", content: "hidden"},
+    {id: 2, name: "arancia_ahh", onlineID:  "arancia_ahh", rank: ranks = 5, status: "online", hash: "#1"},
+    {id: 3, name: "pecete dev", onlineID: "pecete_dev", rank: ranks = 4, status: "online", hash: "#2"},
+    {id: 4, name: "arancia313", onlineID: "arancia313", rank: ranks = 5, status: "online", hash: "#3"},
+    {id: 5, name: "Player 3365", onlineID: "player3365", rank: ranks = 3, status: "online", hash: "#4"}];
+    const bots = [
+    {id: 6, name: "Arancia 3 Network Service Provider", onlineID: "[A3N]", rank: ranks = 2, status: "online", deadlineID: "23479394231798342748", hash: null},
+    {id: 7, name: "celdrone", onlineID: "celdrone", rank: ranks = 0, status: "offline", hash: "#5"},
+    {id: 8, name: "ostash", onlineID: "ostash", rank: ranks = 0, status: "offline", hash: "#6"},
+    {id: 9, name: "Service 1", onlineID: "service-1", rank: ranks = 3, status: "offline", hash: "#7"},
+    {id: 10, name: "Service 2", onlineID: "service-2", rank: ranks = 3, status: "offline", hash: "#8"},
+    {id: 11, name: "Service 3", onlineID: "service-3", rank: ranks = 3, status: "offline", hash: "#9"}];
