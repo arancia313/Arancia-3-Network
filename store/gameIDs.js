28d669b575a900563482bfb149ab6dd0ff7a562d
Arancia 3 Technologies <190222614+arancia313@users.noreply.github.com>
2026-09-08 18:42:43 +0200

    Update gameIDs.js

--- store/gameIDs.js
+++ store/gameIDs.js
@@ -1,3 +1,3 @@
-    const storeID = '32489724892345934'; const store_number = 33; const ishkt = '350279849'; const obr = "495304495"; const games = [
+    console.log('Response from Arancia 3\'s server. Games are loaded properly.'); const storeID = '32489724892345934'; const store_number = 33; const ishkt = '350279849'; const obr = "495304495"; const games = [
     {name: "Incredibox - Smart Hub Kuh treatment", gameID: ishkt, storeID: storeID, store_number: store_number}, 1,
     {name: "Oran's Bay: RP Remastered", gameID: obr, storeID: storeID, store_number: store_number}, 2]
