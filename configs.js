44ff669a596213f01a8bea2e46aed327a6b8174c
Arancia 3 Technologies <marraalan89@gmail.com>
2026-09-08 16:30:59 +0200

    Here, a little deep fix.

--- configs.js
+++ configs.js
@@ -1,2 +1,2 @@
-console.log('Response from Arancia 3\'s servers. Configurations are loaded properly.'); const configs = [
+console.log('Response from Arancia 3\'s Server. Configurations are loaded properly.'); const configs = [
 {certification: true, store: true, netannouncements: true}]
