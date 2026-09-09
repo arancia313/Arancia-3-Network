e801c33f77ef7c984143c42b0fa0b8bb992d74e5
Arancia 3 Technologies <marraalan89@gmail.com>
2026-09-09 09:53:57 +0200

    Corrected a syntax error.

--- live/ao.js
+++ live/ao.js
@@ -1,10 +1,14 @@
+// This is for that authenthicate button.
 function a3function() {
-    let tex;
-    let promp = prompt("Authenthicate as:", "Aoo!");
-    if (promp == null || promp == "") {
-      tex = "Prompt Cancelled.";
-    } else {
-      tex = promp
-    }
-    document.getElementById("authdisp").innerHTML = tex;
+  let tex;
+  let promp = prompt("Authenthicate as:", "Aoo!");
+  if (promp == null || promp == "") {
+  // If you cancelled the prompt, it gives you this.    
+    tex = "Prompt Cancelled.";
+  } else {
+  // If you wrote something on the prompt, it says the tag on the HTML file to display anything that you wrote.
+    tex = promp
+  }
+  // It gets the ID of the thing on the HTML file, inserts the content on it, and displays it.
+  document.getElementById("authdisp").textContent = promp;
 }
--- live/whole.js
+++ live/whole.js
@@ -1,10 +1,10 @@
 /*
-    hint: I use comments to help you identify the function and the algorith of this JavaScript code.
+    hint: I use comments to help you identify the function and the algorithm of this JavaScript code.
     hint: Now, let's just get into it. an editor with dark mode is reccomended for editing this file.
 */
 // Those are settings. You can literally modify them. You can even activate libraries.
 const set = '{"libraries":{"butiwe":{"activate":true},"cve":{"activate":true},"butterfly":{"activate":true}},"settings":{"log":false}}'
-// I'm leaving this comment here to prevent you from accidentaly editing the code on the bottom.
+// I'm leaving this comment here to prevent you from accidentally editing the code on the bottom.
 const formatted = JSON.parse(set);
 // Do NOT edit those, they are custom JavaScript strings from the parsed JSON strings that i made for them to sync.
 let butiwe = formatted.libraries.butiwe.activate
@@ -26,20 +26,6 @@ fetch(url).then(r=>r.text()).then(t=>document.getElementById("view").innerText=t
 const url2 = "https://raw.githubusercontent.com/arancia313/Arancia-3-Network/refs/heads/System/net/platform_updates/A3N_Live/updates.txt";
 fetch(url2).then(r=>r.text()).then(t=>document.getElementById("pa").innerText=t);
 fetch("https://raw.githubusercontent.com/arancia313/Arancia-3-Network/refs/heads/System/users.js").then(r => r.text()).then(eval);
-// This is for that authenthicate button.
-function a3function() {
-    let tex;
-    let promp = prompt("Authenthicate as:", "Aoo!");
-    if (promp == null || promp == "") {
-    // If you cancelled the prompt, it gives you this.    
-      tex = "Prompt Cancelled.";
-    } else {
-    // If you wrote something on the prompt, it says the tag on the HTML file to display anything that you wrote.
-      tex = promp
-    }
-    // It gets the ID of the thing on the HTML file, inserts the content on it, and displays it.
-    document.getElementById("authdisp").textContent = promp;
-}
 // Now, let's get into the console. This is disabled by default for not bothering the console. Just change the log thing on the json thing to be set to true.
 if (logelement == true) {
     console.log("Butiwe is " + butiwe + ".")
