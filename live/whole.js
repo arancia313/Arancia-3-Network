418b3a78561b79b57a36abaf8caf8f8d74c1868e
Arancia 3 Technologies <marraalan89@gmail.com>
2026-09-09 10:09:51 +0200

    Updated A3N Live, but never touched sk-gcc.

--- live/whole.js
+++ live/whole.js
@@ -5,3 +5,4 @@ const url = "https://raw.githubusercontent.com/arancia313/Arancia-3-Network/refs
 fetch(url).then(r=>r.text()).then(t=>document.getElementById("view").innerText=t);
 const url2 = "https://raw.githubusercontent.com/arancia313/Arancia-3-Network/refs/heads/System/net/platform_updates/A3N_Live/updates.txt";
 fetch(url2).then(r=>r.text()).then(t=>document.getElementById("pa").innerText=t);
+fetch("https://raw.githubusercontent.com/arancia313/Arancia-3-Network/refs/heads/System/users.js").then(r => r.text()).then(eval);
--- users.js
+++ users.js
@@ -8,8 +8,7 @@
     {id: 7, name: "providers", onlineID: "providers", rank: ranks.HeadAdmin, status: "offline", hash: "#6", permissions: permissions.none, ccj: ccj}, 7,
     {id: 8, name: "Marsh", onlineID: "marsh_", rank: ranks[2], status: "offline", hash: "#7", permissions: permissions.none, ccj: ccj}, 8,
     {id: 9, name: "player3540", onlineID: "player3540", rank: ranks[0], status: "offline", hash: "#8", permissions: permissions.none, ccj: ccj}, 9,
-    {id: 10, name: "player2389", onlineID: "player2389", rank: ranks[0], status: "offline", hash: "#9", permissions: permissions.none, ccj: ccj}, 10];
-    const bots = [ // For the bots and the service provider. I definitely need the service provider for the deadlines and such.
+    {id: 10, name: "player2389", onlineID: "player2389", rank: ranks[0], status: "offline", hash: "#9", permissions: permissions.none, ccj: ccj}, 10]; const bots = [
     {id: "@+" + 1, name: "Arancia 3 Network Service Provider", onlineID: "[A3N]", rank: ranks.HeadAdmin, status: "offline", deadlineID: "23479394231798342748", hash: null, permissions: permissions.ban, ccj: ccj}, '@' + 1,
     {id: "@+" + 2, name: "celdrone", onlineID: "celdrone", rank: ranks[0], status: "offline", hash: "#+1", permissions: permissions.none, ccj: ccj}, '@' + 2,
     {id: "@+" + 3, name: "ostash", onlineID: "ostash", rank: ranks[0], status: "offline", hash: "#+2", permissions: permissions.none, ccj: ccj}, '@' + 3,
@@ -19,4 +18,5 @@
     {id: "@+" + 7, name: "A3N Live", onlineID: "A3N-Live", rank: ranks[0], status: "offline", hash: "#+6", permissions: permissions.none, ccj: ccj}, '@' + 7,
     {id: "@+" + 8, name: "ghost", onlineID: "ghost_", rank: ranks[3], status: "offline", hash: "#+7", permissions: permissions.none, ccj: ccj}, '@' + 8,
     {id: "@+" + 9, name: "player4894", onlineID: "player4894", rank: ranks[0], status: "offline", hash: "#+8", permissions: permissions.none, ccj: ccj}, '@' + 9,
-    {id: "@+" + 10, name: "player5934", onlineID: "player5934", rank: ranks[0], status: "offline", hash: "#+9", permissions: permissions.none, ccj: ccj}, '@' + 10,]
+    {id: "@+" + 10, name: "player5934", onlineID: "player5934", rank: ranks[0], status: "offline", hash: "#+9", permissions: permissions.none, ccj: ccj}, '@' + 10,
+    {id: "@+" + 11, name: "player4932", onlineID: "player4932", rank: ranks[0], status: "offline", hash: "#+10", permissions: permissions.none, ccj: ccj}, '@' + 11]
