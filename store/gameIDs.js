78dbb9faef59ee910ea162d946dfc186eda6212c
Arancia 3 Technologies <marraalan89@gmail.com>
2026-09-09 10:32:15 +0200

    Aoo!

--- store/gameIDs.js
+++ store/gameIDs.js
@@ -1,4 +1,4 @@
-const formatted = JSON.parse('{"storeID": {"nam":"Arancia 3 Store","storeID": "32489724892345934"}');
+const formatted = JSON.parse('{"storeID": {"nam":"Arancia 3 Store","storeID": "32489724892345934"}}');
 let storeIDo = formatted.storeID.nam;
 console.log('Response from Arancia 3\'s server. Games are loaded properly.');
 console.log('current storeID: ' + storeIDo);
