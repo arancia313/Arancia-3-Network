3563fe5f625d396ba3ed1336ab98e663ba48fbba
Arancia 3 Technologies <marraalan89@gmail.com>
2026-09-09 10:00:33 +0200

    Added another game id. Meanwhile in ISHKT, i released version 1.02. I fixed bad icons, removed some old junk and made some animations.

--- store/gameIDs.js
+++ store/gameIDs.js
@@ -1,3 +1,3 @@
-    console.log('Response from Arancia 3\'s server. Games are loaded properly.'); const storeID = '32489724892345934'; const store_number = 33; const ishkt = '350279849'; const obr = "495304495"; const games = [
-    {name: "Incredibox - Smart Hub Kuh treatment", gameID: ishkt, storeID: storeID, store_number: store_number}, 1,
-    {name: "Oran's Bay: RP Remastered", gameID: obr, storeID: storeID, store_number: store_number}, 2]
+    console.log('Response from Arancia 3\'s server. Games are loaded properly.'); const storeID = '32489724892345934'; const store_number = 33; const ishkt = '350279849'; const obr = "495304495"; const a3d = "809340235"; const games = [
+    {name: "Incredibox - Smart Hub Kuh treatment", gameID: ishkt, storeID: storeID, store_number: store_number},1,{name: "Oran's Bay: RP Remastered", gameID: obr, storeID: storeID, store_number: store_number}, 2,
+    {name: "Arancia 3's Demo", gameID: a3d, storeID: storeID, store_number: store_number},2]
