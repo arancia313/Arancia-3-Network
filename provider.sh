445b661c858f4adfaf94626989ddb27bab488680
Arancia 3 Network Service Provider <arancia3networkserviceprovider@a3services.local>
2026-09-23 17:24:55 +0200

    Aoo!

--- provider.sh
+++ provider.sh
@@ -1,4 +1,5 @@
-git config --global user.name "[A3N]"
+echo "[A3N]: A3N Service Provider is committing."
+git config --global user.name "Arancia 3 Network Service Provider"
 git config --global user.email "arancia3networkserviceprovider@a3services.local"
 git add .
 git commit -m "Aoo!"
