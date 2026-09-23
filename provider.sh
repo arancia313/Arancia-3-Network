a41d7f47985a5c2d8e904afc1f279b82cfac2e45
arancia313 <190222614+arancia313@users.noreply.github.com>
2026-09-23 17:28:51 +0200

    Ditched the provider script.

--- provider.sh
+++
@@ -1,7 +0,0 @@
-echo "[A3N]: A3N Service Provider is committing."
-git config --global user.name "Arancia 3 Network Service Provider"
-git config --global user.email "arancia3networkserviceprovider@a3services.local"
-git add .
-git commit -m "Aoo!"
-git push
-echo "Done."
